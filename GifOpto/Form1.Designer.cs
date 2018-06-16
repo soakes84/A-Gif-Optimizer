4bf7515fd34497e13929f9cbda3b2142f4a50f17
soakes84 <spencer.oakes84@gmail.com>
2018-06-15 21:17:13 -0400

    Browse for gif

--- GifOpto/Form1.Designer.cs
+++ GifOpto/Form1.Designer.cs
@@ -28,33 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSource = new System.Windows.Forms.PictureBox();
+            this.pictureBoxTarget = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.comboBoxCompressLevel = new System.Windows.Forms.ComboBox();
+            this.numericUpDownLossy = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownColor = new System.Windows.Forms.NumericUpDown();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonOptimize = new System.Windows.Forms.Button();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTarget)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLossy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownColor)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pictureBoxSource
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(29, 38);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(304, 281);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pictureBoxSource.BackColor = System.Drawing.Color.White;
+            this.pictureBoxSource.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxSource.Location = new System.Drawing.Point(29, 38);
+            this.pictureBoxSource.Name = "pictureBoxSource";
+            this.pictureBoxSource.Size = new System.Drawing.Size(304, 281);
+            this.pictureBoxSource.TabIndex = 0;
+            this.pictureBoxSource.TabStop = false;
             // 
-            // pictureBox2
+            // pictureBoxTarget
             // 
-            this.pictureBox2.BackColor = System.Drawing.Color.White;
-            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox2.Location = new System.Drawing.Point(487, 38);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(283, 281);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.pictureBoxTarget.BackColor = System.Drawing.Color.White;
+            this.pictureBoxTarget.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxTarget.Location = new System.Drawing.Point(464, 38);
+            this.pictureBoxTarget.Name = "pictureBoxTarget";
+            this.pictureBoxTarget.Size = new System.Drawing.Size(306, 281);
+            this.pictureBoxTarget.TabIndex = 1;
+            this.pictureBoxTarget.TabStop = false;
             // 
             // label1
             // 
@@ -74,19 +90,148 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Optimized Gif";
             // 
+            // comboBoxCompressLevel
+            // 
+            this.comboBoxCompressLevel.FormattingEnabled = true;
+            this.comboBoxCompressLevel.Location = new System.Drawing.Point(98, 12);
+            this.comboBoxCompressLevel.Name = "comboBoxCompressLevel";
+            this.comboBoxCompressLevel.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxCompressLevel.TabIndex = 4;
+            // 
+            // numericUpDownLossy
+            // 
+            this.numericUpDownLossy.Location = new System.Drawing.Point(98, 46);
+            this.numericUpDownLossy.Name = "numericUpDownLossy";
+            this.numericUpDownLossy.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownLossy.TabIndex = 5;
+            // 
+            // numericUpDownColor
+            // 
+            this.numericUpDownColor.Location = new System.Drawing.Point(356, 12);
+            this.numericUpDownColor.Name = "numericUpDownColor";
+            this.numericUpDownColor.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownColor.TabIndex = 6;
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(589, 16);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWidth.TabIndex = 7;
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(589, 48);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHeight.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBoxHeight);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxWidth);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBoxCompressLevel);
+            this.groupBox1.Controls.Add(this.numericUpDownColor);
+            this.groupBox1.Controls.Add(this.numericUpDownLossy);
+            this.groupBox1.Location = new System.Drawing.Point(29, 366);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(741, 91);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Compress Level:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Lossy:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(264, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Color Reduction:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(545, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Width:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(548, 53);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Height:";
+            // 
+            // buttonOptimize
+            // 
+            this.buttonOptimize.Location = new System.Drawing.Point(127, 325);
+            this.buttonOptimize.Name = "buttonOptimize";
+            this.buttonOptimize.Size = new System.Drawing.Size(103, 23);
+            this.buttonOptimize.TabIndex = 10;
+            this.buttonOptimize.Text = "Optimize";
+            this.buttonOptimize.UseVisualStyleBackColor = true;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(258, 12);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 11;
+            this.buttonBrowse.Text = "Browse";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 504);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.buttonOptimize);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBoxTarget);
+            this.Controls.Add(this.pictureBoxSource);
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GIF Optimizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTarget)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLossy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownColor)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,10 +239,23 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBoxSource;
+        private System.Windows.Forms.PictureBox pictureBoxTarget;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxCompressLevel;
+        private System.Windows.Forms.NumericUpDown numericUpDownLossy;
+        private System.Windows.Forms.NumericUpDown numericUpDownColor;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonOptimize;
+        private System.Windows.Forms.Button buttonBrowse;
     }
 }
 
--- GifOpto/Form1.cs
+++ GifOpto/Form1.cs
@@ -16,5 +16,37 @@ namespace GifOpto
         {
             InitializeComponent();
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "GIF Files|*.gif";
+            openFileDialog1.Title = "Select a GIF File";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (var fs = new System.IO.FileStream(openFileDialog1.FileName, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                {
+                    var ms = new System.IO.MemoryStream();
+                    fs.CopyTo(ms);
+                    ms.Position = 0;
+
+                    if (pictureBoxSource.Image != null)
+                        pictureBoxSource.Image.Dispose();
+
+                    pictureBoxSource.Image = Image.FromStream(ms);
+                }
+            }
+        }
     }
 }
